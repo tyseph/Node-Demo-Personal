9795b51cb880686d638561c850d64ee4d85a5034
Sajal Ganjewala <sg748@snu.edu.in>
2022-04-14 15:10:48 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 9999;
+
+app.listen(port, () => {
+    console.log(`Go to http://localhost:${port}/ on browser.`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+    // res.send(`Welcome to my app at ${port}.`);
+    console.log(`My app is running...`);
+});
+
+app.get('/home', (req, res) => {
+    res.send(`Welcome tohome page.`);
+    console.log(`home`);
+});
+
+app.get('/about', (req, res) => {
+    res.send(`Welcome to about page`);
+    console.log(`about`);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`Welcome to contact page`);
+    console.log(`contact`);
+});
